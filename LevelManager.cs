f46a68175ac8783596afd316c24b6015e16625f4
Camila Andrade <camilamdandrade@gmail.com>
2018-01-14 03:46:14 -0300

    Update LevelManager.cs

--- LevelManager.cs
+++ LevelManager.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
-
+	
 	public void LoadLevel(string name){
-		Debug.Log("Level load requested for: " +name);
-		Application.LoadLevel(name);
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
 	}
 	
 	public void QuitRequest(){
-		Debug.Log ("I want to quit!");
-		Application.Quit();
+		Debug.Log ("Quit requested");
+		Application.Quit ();
 	}
 	
-	public void LoadNextLevel(){
+	public void LoadNextLevel() {
 		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void BrickDestoyed() {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
 	}
 }

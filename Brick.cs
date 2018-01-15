51dc0212f194ce00950f968fc354f235f5b4104e
Camila Andrade <camilamdandrade@gmail.com>
2018-01-14 23:41:57 -0300

    Update Brick.cs

--- Brick.cs
+++ Brick.cs
@@ -6,6 +6,7 @@ public class Brick : MonoBehaviour {
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0;
 	public AudioClip crack;
+	public GameObject smoke;
 	
 	private int timesHit;
 	private LevelManager levelManager;
@@ -29,7 +30,7 @@ public class Brick : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D (Collision2D col) {
-		AudioSource.PlayClipAtPoint (crack, transform.position);
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.8f);
 		if (isBreakable) {
 			HandleHits();
 		}
@@ -40,19 +41,32 @@ public class Brick : MonoBehaviour {
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
 			breakableCount--;
+			//Debug.Log(breakableCount);
 			levelManager.BrickDestoyed();
+			PuffSmoke();
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
 		}
 	}
 	
+	//Didn't like the particles :/
+	//void PuffSmoke(){
+		//GameObject smokePuff = Instantiate (smoke, gameObject.transform.position, Quaternion.identity) as GameObject;
+		//smokePuff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+	//}
+	
 	void LoadSprites () {
 		int spriteIndex = timesHit - 1;
 		if (hitSprites[spriteIndex]) {
 			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} else{
+			Debug.LogError("Sprite Missing");
 		}
 	}
-	
-
+		
+	//TODO Remove this
+	void SimulateWin(){
+		levelManager.LoadNextLevel();
+	}
 }
